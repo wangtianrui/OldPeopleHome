718d7316a0b2abbce5b6dc3d3393d61540c2e44b
jiangyuwei666 <739843128@qq.com>
2019-08-19 16:31:30 +0800

    update conf

---
+++ server/OldPeopleHome/src/test/java/com/oldpeoplehome/BaseTest.java
@@ -0,0 +1,19 @@
+package com.oldpeoplehome;
+
+/**
+ * Created By Jiangyuwei on 2019/8/19 15:00
+ * Description:
+ */
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉junit spring配置文件
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
+
+public class BaseTest {
+}
